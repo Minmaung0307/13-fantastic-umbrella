c6b013ff60c7e3f0c1c4755fd2d0a0625a097b1b
Min Maung <minmaung0307@gmail.com>
2022-09-28 17:23:02 -0400

    failed npm test

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,16 @@ const Sequelize = require("sequelize");
 
 const sequelize = process.env.JAWSDB_URL;
 
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: "localhost",
+//       dialect: "mysql",
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
