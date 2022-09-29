680a853d9fcb68f9aa6a7b8599c725857b6cc8b1
Min Maung <minmaung0307@gmail.com>
2022-09-28 21:03:28 -0400

    modified routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -42,10 +42,10 @@ router.get("/:id", async (req, res) => {
 router.post("/", (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      product_name: "Gone With The Wind",
+      price: 80.00,
+      stock: 1,
+      tagIds: [1, 2]
     }
   */
   Product.create(req.body)
@@ -112,8 +112,23 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    // if we get nothing back send 404
+    if (!productData) {
+      res.status(404).json({ message: "No product with this id!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
